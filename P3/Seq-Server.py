0b7c3f446c618c7cea5146a62226810e5881c58b
lagabi12 <g.diazd.2021@alumnos.urjc.es>
2022-03-21 19:26:59 +0100

    P3

--- P3/Seq-Server.py
+++ P3/Seq-Server.py
@@ -1,8 +1,18 @@
 import socket
 from colorama import init, Fore
+from Seq1 import Seq
 
 PORT = 8020
 IP = "127.0.0.1"
+
+seq1 = "ATCGCCTA"
+seq2 = "TCGCCTAA"
+seq3 = "CGCCTAAT"
+seq4 = "GCCTAATC"
+seq0 = "CCTAATCG"
+sequences = (seq0, seq1, seq2, seq3, seq4)
+BASES = ["A", "C", "T", "G"]
+FOLDER = "../P0/P0/sequences/U5"
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -22,17 +32,41 @@ try:
         print("Message from client: {}".format(Fore.LIGHTYELLOW_EX + msg))
 
         # Send the message
-        slices = msg.split(" ")
-        command = slices[0]
+        slice = msg.split(" ")
+        command = slice[0]
+        arg = slice[1]
 
         if command == "PING":
             print("PING command!".format(Fore.LIGHTGREEN_EX))
             response = f"OK!\n"
-            print(response)
 
         elif command == "GET":
-            response = "none"
+            print("GET command!".format(Fore.LIGHTGREEN_EX))
+            if type(arg) == int and 0 <= int(arg) <= 4:
+                arg = int(arg)
+                response = sequences[arg]
+            else:
+                response = "Argument must be an int between 0 and 4"
 
+        elif command == "INFO":
+            length = Seq.len(arg)
+            for base in BASES:
+                bases = base + ": " + Seq.count_base(arg, base)
+            response = length + bases
+
+        elif command == "COMP":
+            response = Seq.complement(arg)
+
+        elif command == "REV":
+            response = Seq.reverse(arg)
+
+        elif command == "GENE":
+            response = Seq.read_fasta2(FOLDER + arg)
+
+        else:
+            response = "Command not available in this server\n"
+
+        print(response)
         send_bytes = str.encode(Fore.LIGHTGREEN_EX + response)
         clientsocket.send(send_bytes)
         clientsocket.close()
--- P3/Test.py
+++ P3/Test.py
@@ -1,7 +1,7 @@
 from Client import Client
-
 IP = "127.0.0.1"
 PORT = 8020
-
 c = Client(IP, PORT)
-print(c.seq_ping())
+
+msg = input("Message for server: ")
+c.debug_talk(msg)
