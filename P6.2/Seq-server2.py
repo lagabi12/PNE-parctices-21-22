218553d86ad81d5546e4e24de0b2d781baa71a7d
lagabi12 <g.diazd.2021@alumnos.urjc.es>
2022-05-22 11:17:38 +0200

    FINAL

--- P6.2/Seq-server2.py
+++ P6.2/Seq-server2.py
@@ -2,109 +2,148 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
-from Seq1 import Seq
 import jinja2 as j
-import urllib.parse as u
+from urllib.parse import parse_qs, urlparse
+from Seq1 import Seq
+
+HTML_FOLDER = "./html/"
+LIST_SEQUENCES = ["ACGTCCAGTAAA", "ACGTAGTTTTTAAACCC", "GGGTAAACTACG",
+                  "CGTAGTACGTA", "TGCATGCCGAT", "ATATATATATATATATATA"]
+LIST_GENES = ["ADA", "FRAT1", "FXN", "RNU5A", "U5"]
+
+def read_html_file(filename):
+    contents = Path(HTML_FOLDER + filename).read_text()
+    contents = j.Template(contents)
+    return contents
+
+#para info
+def count_bases(seq):
+    d = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for b in seq:
+        d[b] += 1
+
+    total = sum(d.values())
+    for k,v in d.items():
+        d[k] = [v, (v * 100) / total]
+    return d
 
 
-PORT = 8080
+def convert_message(base_count): #CONVERTIMOS A STRING PARA MANDAR AL HTML
+    message = ""
+    for k,v in base_count.items():
+        message += k + ": " + str(v[0]) + " (" + str(v[1]) + "%)" +"\n"
+    return message
+
+def info_operation(arg): #PARA AHORRA TIEMPO
+    base_count = count_bases(arg)
+    response = "<p> Sequence: " + arg + "</p>"
+    response += "<p> Total length: " + str(len(arg)) + "</p>"
+    response += convert_message(base_count)
+    return response
+
+# Define the Server's port
+PORT = 22000
+
+# -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
 
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        seq_list = ["ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA","AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA","CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT","CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA","AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT"]
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+        # Print the request line
         termcolor.cprint(self.requestline, 'green')
+        url_path = urlparse(self.path)
+        #urlparse te devuelve trozos del url en tuplas
+        path = url_path.path
+        #path es endpoint
+        arguments = parse_qs(url_path.query)
+        #query es string te da desde la interrogacion hasta el final
+        #parse devuelve diccionario (argumento)
+        print("The old path was", self.path)
+        print("The new path is", url_path.path)
+        print("arguments", arguments)
 
-        try:
-            route = self.requestline.split(" ")[1]
-            filename = route[1:]
-            print("FILENAME", filename)
+        # Message to send back to the clinet
+        if self.path == "/":
+            contents = read_html_file("index.html")\
+                .render(context=
+                        {"n_sequences": len(LIST_SEQUENCES),
+                         "genes": LIST_GENES})
+        elif path == "/ping":
+            contents = read_html_file(path[1:] + ".html").render()
+        elif path == "/get":
+            n_sequence = int(arguments["n_sequence"][0])
+            #key n_sequences te lo inventasdel poner antes del = en html
+            sequence = LIST_SEQUENCES[n_sequence]
+            # path[1:]: html se tiene q llamar igual q path sin barrita
+            contents = read_html_file(path[1:] + ".html")\
+                .render(context = {
+                "n_sequence": n_sequence,
+                "sequence": sequence
+            })
+            #render es read text para html
 
-        except IndexError:
-            route = "/"
-
-        try:
-            filename = filename.split("?")[1]
-            command = filename.split("=")[1]
-
-        except IndexError:
-            pass
-
-        termcolor.cprint(filename, "blue")
-
-        if route == "/" or route == "/favicon.ico":
-            contents = Path("index.html").read_text()
-
-        elif "/ping" in route:
-            contents = Path("PING.html").read_text()
-
-        elif "/get?" in route:
-            n = int(command)
-            seq = seq_list[int(command)]
-            contents = Path("GET.html").read_text().format(n=n, seq=seq)
-
-        elif "/gene?" in route:
-            gene = command
-            seq = Seq()
-            file = "./sequences/" + gene
-            seq.read_fasta(file)
-            gene_seq = ""
-            #QUE NO SALGA TODO SEGUIDO
-            contents = Path("GET.html").read_text().format(n=gene_seq, seq=seq)
-
-        elif "/operation?" in route:
-            command = command
-            arg = command
-            if not Seq(arg).valid_sequence():
-                contents = "Incorrect sequence, please enter a correct sequence"
-
-            elif command == "INFO":
-                new_seq = Seq(arg)
-                n_bases = new_seq.bases()
-                percentages = new_seq.base_percentage()
-                seq_len = new_seq.len()
-
-                contents = "Sequence: " + arg
-
-                contents = contents + "\nTotal lenght: " + str(seq_len) + "\n"
-
-                for k in percentages:
-                    contents = contents + str(k) + ": " + str(n_bases[k]) + " (" + str(percentages[k]) + "%)\n"
-
-            elif command == "COMP":
-                contents = Seq(arg).seq_complement() + "\n"
-
-            elif command == "REV":
-                contents = Seq(arg).seq_reverse() + "\n"
-
-            contents = contents.replace("\n", "<p><p>")
-            contents = Path("OPERATIONS.html").read_text().format(op=command, result=contents, seq=arg)
-
+        elif path == "/gene":
+            gene_name = arguments["gene_name"][0]
+            sequence = Path("./sequences/" + gene_name + ".txt").read_text()
+            contents = read_html_file(path[1:] + ".html") \
+                .render(context={
+                "gene_name": gene_name,
+                "sequence": sequence
+            })
+        elif path == "/operation":
+            sequence = arguments["sequence"][0]
+            operation = arguments["operation"][0]
+            s = Seq(sequence)
+            if operation == "rev":
+                contents = read_html_file(path[1:] + ".html") \
+                    .render(context={
+                    "operation": operation,
+                    "result": s.reverse()
+                })
+            elif operation == "info":
+                contents = read_html_file(path[1:] + ".html") \
+                    .render(context={
+                    "operation": operation,
+                    "result": info_operation(sequence)
+                })
         else:
-            contents = Path("error.html").read_text()
+            contents = "I am the happy server! :-)"
 
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
 
         # Define the content-type header:
-        self.send_header('Content-Type', 'text')
-        self.send_header('Content-Length', len(str.encode(contents)))
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
 
         # The header is finished
         self.end_headers()
 
         # Send the response message
-        self.wfile.write(str.encode(contents))
+        self.wfile.write(contents.encode())
 
         return
 
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
 Handler = TestHandler
 
 # -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
     print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
